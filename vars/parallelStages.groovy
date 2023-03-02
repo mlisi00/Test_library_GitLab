3b8e7d6dfe19fe1ad806bdf7837585c81919d130
Micaela Lisi <m.lisi@reply.it>
2023-03-02 16:23:32 +0100

    Add parallelStages function

--- vars/parallelStages.groovy
+++ vars/parallelStages.groovy
@@ -1,12 +1,25 @@
 def runParallelStages(def stages) {
-    parallelStages = [:]
-    stages.each { stageName ->
-        parallelStages[stageName] = {
-            stage(stageName) {
-                // Esegui le operazioni dello stage
+    def parallelStages = [:]
+    for (e in environments) {
+        deployments[e] = {
+            stage(e) {
                 echo "Esecuzione dello stage ${stageName}"
             }
         }
     }
     parallel parallelStages
 }
+
+/*def environments = ["development", "staging", "production"]
+
+stage("deploy to multiple environments") {
+    def deployments = [:]
+    for (e in environments) {
+        deployments[e] = {
+            stage(e) {
+                echo "Esecuzione dello stage ${stageName}"
+            }
+        }
+    }
+    parallel deployments
+}*/
