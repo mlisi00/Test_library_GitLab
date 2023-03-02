ef52560d2850335a1c8b6759da924c1e6924acff
Micaela Lisi <m.lisi@reply.it>
2023-03-02 17:24:06 +0100

    Fixed parallelStages function

--- vars/parallelStages.groovy
+++ vars/parallelStages.groovy
@@ -1,4 +1,21 @@
 def runParallelStages() {
+    parallel {
+        stage('Stage One') {
+            steps {
+                sh 'echo "Running stage one"'
+            }
+        }
+        stage('Stage Two') {
+            steps {
+                sh 'echo "Running stage two"'
+            }
+        }
+    }
+}
+
+
+/*
+def runParallelStages() {
     pipeline {
         agent { label 'any' }
         stages {
@@ -18,4 +35,4 @@ def runParallelStages() {
             }
         }
     }
-}
+}*/
