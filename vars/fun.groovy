5b8f262a0efe4836b22ac79cc06c0a6634c5ebe7
Micaela Lisi <m.lisi@reply.it>
2023-03-02 11:32:24 +0100

    Fixed JenkinsfileTestLibrary

--- vars/fun.groovy
+++ vars/fun.groovy
@@ -1,11 +1,20 @@
-def incrementVersion(version) {
-    def matcher = version =~ /(\d+)\.(\d+)\.(\d+)/
-    if (matcher.matches()) {
-        def major = Integer.parseInt(matcher.group(1))
-        def minor = Integer.parseInt(matcher.group(2))
-        def patch = Integer.parseInt(matcher.group(3))
-        return "${major}.${minor}.${patch + 1}"
-    } else {
-        return version
-    }
+def incrementVersion(String version) {
+    def parts = version.split("\\.")
+    def major = Integer.parseInt(parts[0])
+    def minor = Integer.parseInt(parts[1])
+    def patch = Integer.parseInt(parts[2])
+    patch += 1 // incrementa il numero di versione di patch
+    return "${major}.${minor}.${patch}"
 }
+
+//def incrementVersion(version) {
+//    def matcher = version =~ /(\d+)\.(\d+)\.(\d+)/
+//    if (matcher.matches()) {
+//        def major = Integer.parseInt(matcher.group(1))
+//        def minor = Integer.parseInt(matcher.group(2))
+//        def patch = Integer.parseInt(matcher.group(3))
+//        return "${major}.${minor}.${patch + 1}"
+//    } else {
+//        return version
+//    }
+//}
