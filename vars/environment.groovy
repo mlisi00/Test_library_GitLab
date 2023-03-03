d9717ec7fca690446e10ef5a261938c636f14a15
Micaela Lisi <m.lisi@reply.it>
2023-03-03 11:20:47 +0100

    Added testFunction

--- vars/environment.groovy
+++ vars/environment.groovy
@@ -3,9 +3,11 @@ def testFunction(envTarget) {
         case 'dev':
             projecta = "dev-site1";
             projectb = "dev-site2";
+            break;
         case 'test':
             projecta = "test-site1";
             projectb = "test-site2";
+            break;
     }
     return [projecta, projectb]
 }
